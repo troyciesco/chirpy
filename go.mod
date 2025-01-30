8a7fac6c14def1e03d98c8067c0b0296ae6b080c
Troy Ciesco <tmciesco@gmail.com>
2025-01-29 20:43:19 -0500

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/troyciesco/chirpy
+
+go 1.23.5
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	server := http.Server{Addr: ":8080", Handler: mux}
+
+	server.ListenAndServe()
+}
