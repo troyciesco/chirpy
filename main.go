0112cbce1bdf8df8f21c3f02a5fecb71f23aa013
Troy Ciesco <tmciesco@gmail.com>
2025-01-30 11:25:56 -0500

    add healthcheck

--- main.go
+++ main.go
@@ -6,8 +6,12 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	mux.Handle("/assets", http.FileServer(http.Dir("/assets")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 	server := http.Server{Addr: ":8080", Handler: mux}
 
 	server.ListenAndServe()
