d38b8a3b08a45017cbceec389e74a7fdcfc8b573
Troy Ciesco <tmciesco@gmail.com>
2025-01-29 21:06:35 -0500

    add v basic file serving

--- main.go
+++ main.go
@@ -6,7 +6,17 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/assets", http.FileServer(http.Dir("/assets")))
 	server := http.Server{Addr: ":8080", Handler: mux}
 
 	server.ListenAndServe()
 }
+
+// srv := &http.Server{
+// 	Addr:    ":" + port,
+// 	Handler: mux,
+// }
+
+// log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+// log.Fatal(srv.ListenAndServe())
